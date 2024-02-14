240b94860e5b42e7fcad2bff5a415be7b44b4c06
dominhdang148 <myclone235@gmail.com>
2024-02-14 23:49:50 +0700

    update Water session - add plus and minus button

--- app/screens/home/components/WaterBox.tsx
+++ app/screens/home/components/WaterBox.tsx
@@ -1,3 +1,4 @@
+import { AntDesign } from '@expo/vector-icons'
 import React, { useState } from 'react'
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import WaterDropSvg from '../../../../assets/images/water_drop.svg'
@@ -9,20 +10,35 @@ export default function WaterBox() {
     const [drinkTime, setDrinkTime] = useState(0);
 
     return (
-        <TouchableOpacity style={styles.boxContainer} onPress={() => { setDrinkTime(drinkTime + 250) }}>
+        <View style={styles.boxContainer}>
             <View style={styles.textContainer}>
-                <Text style={styles.boxTitle}>Uống nước</Text>
-                <Text style={styles.boxData}>1,5 Lit</Text>
+                <Text style={styles.boxTitle}>Đã uống</Text>
+                <Text style={styles.boxData}>{drinkTime < 1000 ? drinkTime : drinkTime / 1000} {drinkTime < 1000 ? "ml" : "L"} </Text>
                 <Text style={styles.boxSubtitle}>1 giọt nước = 250ml </Text>
             </View>
-            <View style={styles.iconContainer}>
-                {
-                    waterDrops.map(value => (
-                        <WaterDropSvg width={26} height={26} key={String(value)} fill={drinkTime < value ? MainColors[50] : SubColors.blue} />
-                    ))
-                }
+            <View style={styles.waterCounterContainer}>
+
+                {/* Water Icon session */}
+                <View style={styles.iconContainer}>
+                    {
+                        waterDrops.map(value => (
+                            <WaterDropSvg width={26} height={26} key={String(value)} fill={drinkTime < value ? MainColors[50] : SubColors.blue} />
+                        ))
+                    }
+                </View>
+
+                {/* Button Session */}
+                <View style={styles.buttonContainer}>
+                    <TouchableOpacity onPress={() => { setDrinkTime(drinkTime - 250) }} disabled={drinkTime == 0}>
+                        <AntDesign name="minus" size={24} color={drinkTime == 0 ? MainColors[50] : SubColors.blue} style={{ paddingHorizontal: 8 }} />
+                    </TouchableOpacity>
+                    <WaterDropSvg width={24} height={26} fill={SubColors.blue} />
+                    <TouchableOpacity onPress={() => { setDrinkTime(drinkTime + 250) }} disabled={drinkTime == 1500}>
+                        <AntDesign name="plus" size={24} color={drinkTime == 1500 ? MainColors[50] : SubColors.blue} style={{ paddingHorizontal: 8 }} />
+                    </TouchableOpacity>
+                </View>
             </View>
-        </TouchableOpacity>
+        </View>
     )
 }
 
@@ -35,6 +51,11 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         justifyContent: 'space-between'
     },
+    waterCounterContainer: {
+        flex: 2,
+        marginLeft: 16,
+        marginRight: 16,
+    },
     textContainer: {
         marginTop: 8,
         marginBottom: 15,
@@ -44,9 +65,12 @@ const styles = StyleSheet.create({
         flex: 2,
         alignItems: 'center',
         justifyContent: 'space-around',
-        marginLeft: 16,
-        marginRight: 16,
         flexDirection: 'row',
+    },
+    buttonContainer: {
+        marginVertical: 8,
+        flexDirection: 'row',
+        justifyContent: 'center',
     },
     boxTitle: {
         fontFamily: Raleway.semibold,
